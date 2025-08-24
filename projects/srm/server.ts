95f8869a4e8c1684fad2539c9102ce2658653796
arielrepos <ariel_ohana@webiks.com>
2025-08-24 10:53:35 +0300

    push to stage

--- projects/srm/server.ts
+++ projects/srm/server.ts
@@ -94,7 +94,6 @@ function run(): void {
   });
 }
 
-
 // Webpack will replace 'require' with '__webpack_require__'
 // '__non_webpack_require__' is a proxy to Node 'require'
 // The below code is to ensure that the server is run only when not requiring the bundle.
